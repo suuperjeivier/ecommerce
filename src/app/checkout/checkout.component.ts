0656d95aa1fe0434e3975137a7f4de84896420fc
LuisBalan <jlbalcz@gmail.com>
2025-01-31 23:56:10 -0600

    checkout component and some padding details

--- src/app/checkout/checkout.component.ts
+++ src/app/checkout/checkout.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputNumberModule } from 'primeng/inputnumber';
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [ InputGroupModule, InputGroupAddonModule, InputNumberModule ],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css'
 })
