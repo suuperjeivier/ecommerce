7216ce3ab938dcaa46fa931a3875ad05080f90d8
Javier Brito Pacheco (Javi) <javier.pacheco2@ibm.com>
2025-02-12 18:55:56 -0600

    feature: added animations on home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import {StyleClassModule} from 'primeng/styleclass';
 import {RippleModule} from 'primeng/ripple';
 import {ButtonModule} from 'primeng/button';
+import { AnimateOnScrollModule } from 'primeng/animateonscroll';
 
 @Component({
   selector: 'app-home',
-  imports: [RippleModule, ButtonModule, StyleClassModule],
+  imports: [RippleModule, ButtonModule, StyleClassModule, AnimateOnScrollModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
